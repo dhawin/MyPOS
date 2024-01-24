e55f02a1430b70cd457890295d2fc93d3c8785f0
dhawin.k <dhawin.k@KB-19LBK10B67>
2024-01-24 14:28:27 +0700

    save

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
